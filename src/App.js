22c89d44d939dbae49554f040b8089b4256ccbba
Juraj Urban <durko11169@gmail.com>
2022-02-07 21:15:21 +0100

    add goal list and form to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { useState } from "react";
+import GoalInput from "./components/Goals/GoalInput/GoalInput";
+import GoalsList from "./components/Goals/GoalsList/GoalsList";
 import Login from "./components/Login/Login";
 
 import MainHeader from "./components/MainHeader/MainHeader";
+import Card from "./components/UI/Card/Card";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [goals, setGoals] = useState([]);
+
+  const addGoalHandler = (enteredText) => {
+    setGoals((prevGoals) => {
+      const updatedGoals = [...prevGoals];
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      return updatedGoals;
+    });
+  };
+
+  const deleteItemHandler = (goalId) => {
+    setGoals((prevGoals) => {
+      const updatedGoals = prevGoals.filter((goal) => goal.id !== goalId);
+      return updatedGoals;
+    });
+  };
 
   const loginHandler = () => {
     setIsLoggedIn(true);
@@ -14,10 +33,23 @@ function App() {
     setIsLoggedIn(false);
   };
 
+  let content = <p>No goals found.</p>;
+
+  if (goals.length > 0)
+    content = <GoalsList goals={goals} onDeleteItem={deleteItemHandler} />;
+
   return (
     <React.Fragment>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>{!isLoggedIn && <Login onLogin={loginHandler} />}</main>
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && (
+          <Card>
+            <GoalInput onAddGoal={addGoalHandler} />
+          </Card>
+        )}
+        {isLoggedIn && <Card>{content}</Card>}
+      </main>
     </React.Fragment>
   );
 }
